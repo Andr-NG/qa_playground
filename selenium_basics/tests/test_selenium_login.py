5efa1ee6acf226dad53a01fc6e2ff7148bcd9fa3
Andr-NG <andreynguyen467@gmail.com>
2025-11-11 16:14:39 +0700

    updating test

--- selenium_basics/tests/test_selenium_login.py
+++ selenium_basics/tests/test_selenium_login.py
@@ -1,3 +1,4 @@
+import time
 from selenium_basics.pages.selenium_login_page import LoginPage
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.webdriver import WebDriver
@@ -17,20 +18,36 @@ def test_selenium_login(login_page: LoginPage, driver: WebDriver) -> None:
             "Request body",
             attachment_type=allure.attachment_type.TEXT,
         )
+        login_page.open("https://app.multilogin.com/en/auth/signin")
         login_page.log_in("andrey.nguyen@multilogin.com", "Qwerty123!")
-    wait = WebDriverWait(driver, timeout=2)
 
-    with allure.step("Closing the referral banner"):
-        wait.until(
-            EC.element_to_be_clickable((By.CSS_SELECTOR, ".close.cursor-pointer"))
-        ).click()
-        allure.attach(
-            driver.get_screenshot_as_png(),
-            "home_page_screenshot",
-            allure.attachment_type.PNG,
-        )
+    # wait = WebDriverWait(driver, timeout=4)
+
+    # with allure.step("Closing the referral banner"):
+    #     wait.until(
+    #         EC.element_to_be_clickable((By.CSS_SELECTOR, ".close.cursor-pointer"))
+    #     ).click()
+    #     allure.attach(
+    #         driver.get_screenshot_as_png(),
+    #         "home_page_screenshot",
+    #         allure.attachment_type.PNG,
+    #     )
 
     with allure.step("Asserting"):
+    #     wait.until(
+    #         EC.presence_of_element_located(
+    #             (By.XPATH, '//div[@class="sidebar-container-logo"]')
+    #         )
+    #     )
+    #     driver.execute_cdp_cmd(
+    #         "Browser.setPermission",
+    #         {
+    #             "permission": {"name": "local-network"},
+    #             "setting": "granted",  # or "denied"
+    #             "origin": "https://app.multilogin.com",
+    #         },
+    #     )
+        time.sleep(2)
         home_page = driver.current_url
         title = driver.title
         assert home_page == "https://app.multilogin.com/en/home/profiles"
